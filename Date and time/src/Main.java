3a8efe68e6784d61f0083ebb8f90546c859767c0
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-01-30 23:11:17 +0200

    ,

--- Date and time/src/Main.java
+++ Date and time/src/Main.java
@@ -1,16 +1,27 @@
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
+        // ways of getting dates
 
+       /*
         LocalDateTime now = LocalDateTime.now();
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String newDate = now.format(formatter);
+        System.out.println(newDate);
+        */
 
-        String newDateTime = now.format(formatter);
+        /*
+        LocalDateTime date1 = LocalDateTime.of(2024,12,25,12,0,0);
+        LocalDateTime date2 = LocalDateTime.of(2025,1,1,0,0,0);
 
-        System.out.println(newDateTime);
+        if (date1.isBefore(date2)) {
+            System.out.println(date1 +" " + "is before " + date2);
+        }
+        else if (date1.isAfter(date2)) {
+            System.out.println(date1 +" " + "is after " + date2);
+        }
+         */
     }
 }
